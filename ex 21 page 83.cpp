635441013ea37739ea9291b0b9b7ca12e84ad1ff
amirahnazli <33883963+amirahnazli@users.noreply.github.com>
2018-01-15 09:54:24 +0800

    Add files via upload
    exercise 21

---
+++ ex 21 page 83.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	// Local variable declaration;
+	int a=10;
+	
+	//do loop execution
+	do
+	{
+		cout<<"Value of a : "<< a <<endl;
+		a = a + 1;
+	}
+	
+	while (a < 20);
+	
+	return 0;
+}
